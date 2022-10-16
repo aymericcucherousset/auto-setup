84b23e5a412fcd1eda1631f4573235174a89e1c1
aymericcucherousset <aymeric.cucherousset@gmail.com>
2022-10-16 19:14:48 +0200

    Adding exit option & moving to the script folder

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# Move the the script directory
+cd "$(dirname "$0")"
+
 FILES='softwares/*.sh'
 i=1
 
 printf "\nChose the software you want to install:\n\n"
+printf "\n0. Exit\n"
 
 for f in $FILES
 do
@@ -12,6 +17,11 @@ done
 
 printf "\n"
 read -p "Enter the number of the software you want to install: " num
+
+# If the number is 0, exit the script
+if [ $num = 0 ]; then
+  exit 0
+fi
 
 # Getting the file name
 file=$(ls $FILES | sed -n "$num"p)
